4321eec89f38d40acb6f7d79eb1a2f1bd152e270
Picazo27 <greciapicazoxd@gmail.com>
2024-03-11 11:36:32 -0600

    cambios json

---
+++ start/routes/UserRoutes.ts
@@ -0,0 +1,12 @@
+// start/routes.ts
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('/', 'UserController.index').middleware('auth:api')
+  Route.get('/:user_id', 'UserController.show')
+  Route.post('/', 'UserController.store')
+  Route.put('/:user_id', 'UserController.update')
+  Route.delete('/:user_id', 'UserController.destroy')
+  Route.post('/login', 'UserController.authLogin')
+}).prefix('/api/users')
