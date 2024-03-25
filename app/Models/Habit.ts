a9ce38bb6a513ac4f2a37c867422fcedf63e0779
VictorIsaias <isaias.castrejonrdz@gmail.com>
2024-03-25 12:41:01 -0600

    habitsController & ConfigurationController

---
+++ app/Models/Habit.ts
@@ -0,0 +1,39 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo,BelongsTo, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Configuration from './Configuration'
+
+export default class Habit extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  public static table = "Habitos"
+
+  @column()
+  public user_id: number 
+
+  @column()
+  public name: String 
+
+  @column()
+  public description: String 
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+  
+  @belongsTo(() => User, {
+    localKey: 'user_id',  
+    foreignKey: 'id',
+  })
+  public user: BelongsTo<typeof User>
+
+  @hasMany(() => Configuration, {
+    localKey: 'id',  
+    foreignKey: 'habit_id',
+  })
+  public configuration: BelongsTo<typeof Configuration>
+
+}
