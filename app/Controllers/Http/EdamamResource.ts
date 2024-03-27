19eee687da13da053ecf29395620dcb4e7f2c318
Picazo27 <greciapicazoxd@gmail.com>
2024-03-27 10:00:35 -0600

    cambios

--- app/Controllers/Http/EdamamResource.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default class EdamamaResource {
-    public static async comida(){
-        const res = await axios.get('url')
-        return res.data
-    }
-}
