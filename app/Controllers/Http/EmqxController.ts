cc3ac493372009c54529b671de3a5352ab3ae070
Picazo27 <greciapicazoxd@gmail.com>
2024-03-27 23:18:11 -0600

    cambios

--- app/Controllers/Http/EmqxController.ts
+++ app/Controllers/Http/EmqxController.ts
@@ -6,7 +6,7 @@ import Env from '@ioc:Adonis/Core/Env'
 export default class EmqxController {
 /**
  * @swagger
- * /api/emqx/publishEMQXTopic:
+ * /api/emqx/publishEMQXTopic1:
  *   post:
  *     tags:
  *       - EMQX
@@ -68,7 +68,7 @@ export default class EmqxController {
  *               type: object
  *               example: { error: "Mensaje de error" }
  */
-public async publishEMQXTopic({ response }: HttpContextContract) {
+public async publishEMQXTopic1({ response }: HttpContextContract) {
     try {
         const url = 'http://143.198.135.231:18083/api/v5/publish';
         const payload = {
@@ -125,7 +125,7 @@ public async publishEMQXTopic({ response }: HttpContextContract) {
 *     produces:
 *       - application/json
 *     parameters:
-*       - name: topic_name
+*       - name: topic
 *         in: query
 *         required: true
 *         schema:
@@ -155,33 +155,141 @@ public async publishEMQXTopic({ response }: HttpContextContract) {
 */
 public async getEMQXTopic({ request, response }: HttpContextContract) {
   try {
-    const topicName = request.input('topic_name');
-    const url = Env.get('MQTT_HOST') + '/mqtt/retainer/message/' + topicName;
+    const topic = request.input('topic_name');
+    const url = Env.get('MQTT_HOST') + '/mqtt/retainer/message/' + topic;
 
     const axiosResponse = await axios.get(url, {
       auth: {
         username: Env.get('MQTT_API_KEY'),
         password: Env.get('MQTT_SECRET_KEY')
       }
-    }).catch((error) => error);
+    });
 
-    if (!axiosResponse.status && axiosResponse.response.status !== 200) {
-      return response.status(axiosResponse.response.status).send({
+    if (axiosResponse.status !== 200) {
+      return response.status(axiosResponse.status).send({
+        title: 'Error',
+        message: 'Ocurrió un error al obtener el mensaje retenido más reciente.',
+        type: 'error',
+        data: {
+          error: axiosResponse.statusText
+        },
+      });
+    }
+
+    const retainedMessage = axiosResponse.data;
+    
+    return response.status(200).send({
+      title: 'Mensaje retenido más reciente obtenido con éxito',
+      message: 'El último mensaje retenido del tema ha sido recuperado correctamente.',
+      type: 'success',
+      data: {
+        retained_message: retainedMessage
+      },
+    });
+  } catch (error) {
+    let errorMessage = 'Ocurrió un error interno al procesar la solicitud.';
+    if (error.response) {
+      errorMessage = `Se recibió una respuesta con el estado ${error.response.status}: ${error.response.statusText}`;
+    } else if (error.request) {
+      errorMessage = 'No se recibió ninguna respuesta del servidor.';
+    } else {
+      errorMessage = `Error al realizar la solicitud: ${error.message}`;
+    }
+    return response.status(500).send({
+      title: 'Error',
+      message: errorMessage,
+      type: 'error',
+      data: {
+        error: error.message
+      },
+    });
+  }
+}
+
+ /**
+    * @swagger
+    * /api/emqx/publishEMQXTopic:
+    *   post:
+    *     tags:
+    *       - MQTT
+    *     produces:
+    *       - application/json
+    *     requestBody:
+    *       content:
+    *         application/json:
+    *           schema:
+    *             type: object
+    *             properties:
+    *               topic_name:
+    *                 topic: string
+    *               topic_message:
+    *                 topic: string
+    *             required:
+    *               - topic_name
+    *               - topic_message
+    *     responses:
+    *       200:
+    *         description: Todo salió bien cuando mandamos este estatus
+    *         content:
+    *           application/json:
+    *             schema:
+    *               type: object
+    *               properties:
+    *                 type:
+    *                   type: string
+    *                 title:
+    *                   type: string
+    *                   description: Titulo de la respuesta
+    *                 message:
+    *                   type: string
+    *                 data:
+    *                   type: object
+    *                   description: Datos de respuesta
+    *                   properties:
+    *                     user:
+    *                       type: object
+    *                       $ref: '#/components/schemas/User'
+    */
+public async publishEMQXTopic({ request, response }: HttpContextContract) {
+  try {
+    const body = request.all()
+    const url = Env.get('MQTT_HOST') + '/publish'
+    const payload = {
+      "payload_encoding": "plain",
+      "topic": body.topic_name,
+      "qos": 0,
+      "payload": body.topic_message,
+      "properties": {
+        "user_properties": {
+          "foo": "bar"
+        }
+      },
+      "retain": true
+    }
+
+    const res = await axios.post(url, payload, {
+      auth: {
+        username: Env.get('MQTT_API_KEY'),
+        password: Env.get('MQTT_SECRET_KEY')
+      }
+    }).catch((error) => error)
+    if (!res.status && res.response.status !== 202) {
+      return response.status(res.response.status).send({
         title: 'Error',
         message: 'Ocurrio un error',
         type: 'error',
         data: {
-          error: axiosResponse.response
+          error: res.response
         },
-      });
+      })
     }
 
     return response.status(200).send({
       title: 'Topico enviado',
       message: '',
       type: 'success',
-      data: {...axiosResponse.data, message_decoded: atob(axiosResponse.data.payload)},
-    });
+      data: res.data,
+    })
 
   } catch (error) {
     return response.status(500).send({
@@ -191,10 +299,9 @@ public async getEMQXTopic({ request, response }: HttpContextContract) {
       data: {
         error: error.message
       },
-    });
+    })
   }
 }
-
 
 public async webhookRes ({ request, response }: HttpContextContract) {
   const body = request.all()
