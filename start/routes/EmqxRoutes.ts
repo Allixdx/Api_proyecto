d81400e7cf2011887f64ef63f7c335ef1bb2fe8b
Picazo27 <greciapicazoxd@gmail.com>
2024-03-28 19:18:17 -0600

    cambios

--- start/routes/EmqxRoutes.ts
+++ start/routes/EmqxRoutes.ts
@@ -2,7 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
     
-    Route.post('/publishEMQXTopic1','EmqxController.publishEMQXTopic1')
+    Route.post('/publishEMQXTopic','EmqxController.publishEMQXTopic')
     Route.post('/topic-retained','EmqxController.getEMQXTopic')
     Route.post('/obtenerDistancia','EmqxController.obtenerDistancia')
     Route.post('/obtenerPasos','EmqxController.obtenerPasos')
